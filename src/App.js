42d14a2233021b3a1f3addb4f902fd1609f507d9
tatiana faramarzi <tfaramarzi@gmail.com>
2019-08-26 18:13:04 -0400

    built admin login form, successfully fetching token from API, created axiosWithAuth header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 
+//components
+import LoginForm from "./components/Login";
+
 function App() {
   return (
     <div className="App">
+      <h1>WORK IN PROGRESS</h1>
+      <LoginForm />
       
     </div>
   );
