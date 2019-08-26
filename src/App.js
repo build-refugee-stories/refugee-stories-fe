a8c01e886e3d5841f34894787f76cbf084266ee9
tatiana faramarzi <tfaramarzi@gmail.com>
2019-08-26 18:55:33 -0400

    Crystal and Tatiana: built admin dashboard component and adminsForReview child component, added and tested private route to admin dashboard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 
 //components
 import LoginForm from "./components/Login";
+import PrivateRoute from "./components/admin-components/PrivateRoute";
+import Dashboard from './components/admin-components/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <h1>WORK IN PROGRESS</h1>
       <LoginForm />
+      <PrivateRoute path="/dashboard" component={Dashboard}/>
       
     </div>
   );
