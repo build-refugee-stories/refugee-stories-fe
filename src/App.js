baea94bd7d449d3ba78ee747f5bf9c8ca9228976
Sara Alkhamri <s.alkhamri@gmail.com>
2019-08-26 16:32:46 -0700

    Created Signup form and imported it to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import SignupForm from './components/Signup';
 
 function App() {
   return (
     <div className="App">
-      
+      <SignupForm/>
     </div>
   );
 }
