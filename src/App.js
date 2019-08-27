2467d8f968405865184b319af1b809b20b995af4
Joscelyn <JoscelynRose@gmail.com>
2019-08-27 17:09:24 -0400

    finished styling stories page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Login from './components/Login';
-
+import StoryForm from './components/StoryForm.js';
 import { Route } from 'react-router-dom';
 import AdminConfirmation from './components/confirmations/AdminConfirmation.js';
 import StoryConfirmation from './components/confirmations/StoryConfirmation.js';
@@ -13,7 +13,7 @@ import Dashboard from './components/admin-components/Dashboard';
 function App() {
   return (
     <div className="App">
-      <StoryConfirmation />
+      <StoryForm />
     </div>
   );
 }
