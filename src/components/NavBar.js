170423fcc5f083069f03b6e9c409d556fbc86835
Joscelyn <JoscelynRose@gmail.com>
2019-08-27 13:11:53 -0400

    added hamburger menu functionality. fixed some styling for media queries

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import { Breadcrumb } from 'semantic-ui-react';
-import { NavLink } from 'react-router-dom';
-import HomeView from './HomeView.js';
-import StoryForm from './StoryForm.js';
-import Login from './Login.js';
+import React, { useState } from 'react';
 
-const NavBar = () => (
-  <div className="topnav">
-    <NavLink className="nav-item">Home</NavLink>
-    <NavLink className="nav-item" href="#">
-      About
-    </NavLink>
-    <NavLink className="nav-item">Contribute</NavLink>
-    <NavLink className="nav-item">Admin</NavLink>
-  </div>
-);
+import { NavLink } from 'react-router-dom';
+import whiteHamburger from '../assets/white-hamburger-menu.png';
+
+function NavBar() {
+  const [isActive, setIsActive] = useState(false);
+
+  function handleClick() {
+    setIsActive(!isActive);
+  }
+  return (
+    <div className="navbarcontainer">
+      <div className="topnav">
+        <NavLink className="nav-item">Home</NavLink>
+        <NavLink className="nav-item" href="#">
+          About
+        </NavLink>
+        <NavLink className="nav-item">Contribute</NavLink>
+        <NavLink className="nav-item">Admin</NavLink>
+      </div>
+      <div className="dropdown">
+        <img
+          onClick={handleClick}
+          className="white-hamburger"
+          alt="hamburger menu"
+          src={whiteHamburger}
+        />
+        <div className={isActive ? 'dropdown-content' : 'dropdown-off'}>
+          <NavLink className="dropdown-link" target="_blank">
+            Home
+          </NavLink>
+          <NavLink className="dropdown-link" target="_blank">
+            About
+          </NavLink>
+          <NavLink className="dropdown-link" target="_blank">
+            Contribute
+          </NavLink>
+          <NavLink className="dropdown-link" target="_blank">
+            Admin
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default NavBar;
