0927583f10c9f290b5358b8d5865f46e2611041b
Sara Alkhamri <s.alkhamri@gmail.com>
2019-08-26 20:41:31 -0700

    Added destructured errors prop and validation

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Form, Field, withFormik } from 'formik';
+import * as Yup from "yup";
 
 const SignupForm = ({ errors, touched }) => {
     return (
         <div className="signup-form">
             <Form>
                 <Field name="username" type="text" placeholder="Username" />
+                {/* Adding destructured 'errors' prop to the form */}
+                {touched.username && errors.username && <p>{errors.username}</p>}
                 <Field name="password" type="password" placeholder="Password"/>
-                <button>Submit</button>
+                {touched.password && errors.password && <p>{errors.password}</p>}
+                <button type="submit">Submit</button>
             </Form>
         </div>
     )
@@ -19,6 +23,20 @@ const FormikSignupForm = withFormik ({
             username: username || "",
             password: password || "",
         };
+    },
+
+    // Validation
+    validationSchema: Yup.object().shape({
+        username: Yup.string()
+        .required("Username Required"),
+
+        password: Yup.string()
+        .min(6)
+        .required("Password Required")
+    }),
+
+    handleSubmit(values) {
+        console.log(values)
     }
 })(SignupForm);
 
