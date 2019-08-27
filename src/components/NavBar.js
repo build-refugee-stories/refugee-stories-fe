c352795de3fd10a6757b6f541e0fd12304f936c8
Joscelyn <JoscelynRose@gmail.com>
2019-08-26 21:16:01 -0400

    added styling for buttons and form fields

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,18 +7,12 @@ import Login from './Login.js';
 
 const NavBar = () => (
   <div className="topnav">
-    <NavLink className="nav-item" to={HomeView}>
-      Home
-    </NavLink>
+    <NavLink className="nav-item">Home</NavLink>
     <NavLink className="nav-item" href="#">
       About
     </NavLink>
-    <NavLink className="nav-item" to={StoryForm}>
-      Contribute
-    </NavLink>
-    <NavLink className="nav-item" to={Login}>
-      Admin
-    </NavLink>
+    <NavLink className="nav-item">Contribute</NavLink>
+    <NavLink className="nav-item">Admin</NavLink>
   </div>
 );
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import { Form, Field, withFormik } from 'formik';
+import NavBar from './NavBar.js';
 
 const SignupForm = ({ errors, touched }) => {
-    return (
-        <div className="signup-form">
-            <Form>
-                <Field name="username" type="text" placeholder="Username" />
-                <Field name="password" type="password" placeholder="Password"/>
-                <button>Submit</button>
-            </Form>
-        </div>
-    )
+  return (
+    <div>
+      <NavBar />
+      <Form className="signup-form">
+        <Field
+          className="form-field"
+          name="username"
+          type="text"
+          placeholder="Username"
+        />
+        <Field
+          className="form-field"
+          name="password"
+          type="password"
+          placeholder="Password"
+        />
+        <button className="submit-button">Submit</button>
+      </Form>
+    </div>
+  );
 };
 
-const FormikSignupForm = withFormik ({
-    mapPropsToValues({ username, password }) {
-        return {
-            username: username || "",
-            password: password || "",
-        };
-    }
+const FormikSignupForm = withFormik({
+  mapPropsToValues({ username, password }) {
+    return {
+      username: username || '',
+      password: password || ''
+    };
+  }
 })(SignupForm);
-
 
 export default FormikSignupForm;
