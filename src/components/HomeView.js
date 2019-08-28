6b927068ff4f8c01aa2db3f1f66d3ce612395259
Joscelyn <JoscelynRose@gmail.com>
2019-08-28 08:11:34 -0400

    WIP: styling the home page

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -3,6 +3,9 @@ import { axiosWithAuth } from '../utils/axiosWithAuth';
 import StoryView from './StoryView';
 import Header from './Header';
 import Footer from './Footer';
+import heroImage from '../assets/story-2.jpg';
+import logo from '../assets/white-logo.png';
+import hamburger from '../assets/white-hamburger-menu.png';
 
 const Homeview = () => {
   const [storyList, setStoryList] = useState([]);
@@ -23,7 +26,16 @@ const Homeview = () => {
 
   return (
     <div className="home-container">
-      <Header />
+      <img src={heroImage} className="hero-image" />
+      <img src={logo} className="white-logo" />
+      <img src={hamburger} className="home-white-hamburger" />
+      <div className="hero-text">
+        <h1 className="hero-title">Refugee Stories</h1>
+        <p className="hero-p">
+          “The world will not be destroyed by those who do evil, but by those
+          who watch them without doing anything.” — Albert Einstein
+        </p>
+      </div>
       <StoryView stories={storyList} />
       <Footer />
     </div>
