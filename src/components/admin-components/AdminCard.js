c1f0024ecb7c0e5314104152092dbe9ea064c42d
Joscelyn <JoscelynRose@gmail.com>
2019-08-29 19:44:22 -0400

    added placeholder images for stories that don't hae a picture

--- src/components/admin-components/AdminCard.js
+++ src/components/admin-components/AdminCard.js
@@ -8,40 +8,49 @@ import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
-    button: {
-      margin: theme.spacing(1),
-    },
-    leftIcon: {
-      marginRight: theme.spacing(1),
-    },
-    rightIcon: {
-      marginLeft: theme.spacing(1),
-    },
-    iconSmall: {
-      fontSize: 20,
-    },
-  }));
-  
-    
-const AdminCard = (props) => {
+  button: {
+    margin: theme.spacing(1)
+  },
+  leftIcon: {
+    marginRight: theme.spacing(1)
+  },
+  rightIcon: {
+    marginLeft: theme.spacing(1)
+  },
+  iconSmall: {
+    fontSize: 20
+  }
+}));
 
-    const classes = useStyles();
+const AdminCard = props => {
+  const classes = useStyles();
 
-    return (
-        <div className='admin-card'>
-            <h5>{props.firstName} {props.lastName}</h5>
-            <p>Email: {props.email}</p>
-            <Button onClick={() => props.approve(props.id)} variant="contained" color="primary" className={classes.button}>
-                    Approve
-                    <CheckBoxIcon className={classes.rightIcon} />
-            </Button>
-            <Button onClick={() => props.reject(props.id)} variant="contained" color="primary" className={classes.button}>
-                    Reject
-                    <BlockIcon className={classes.rightIcon} />
-            </Button>
-        </div>
-    )
-}
+  return (
+    <div className="admin-card">
+      <h5>
+        {props.firstName} {props.lastName}
+      </h5>
+      <p>Email: {props.email}</p>
+      <Button
+        onClick={() => props.approve(props.id)}
+        variant="contained"
+        color="primary"
+        className={classes.button}
+      >
+        Approve
+        <CheckBoxIcon className={classes.rightIcon} />
+      </Button>
+      <Button
+        onClick={() => props.reject(props.id)}
+        variant="contained"
+        color="primary"
+        className={classes.button}
+      >
+        Reject
+        <BlockIcon className={classes.rightIcon} />
+      </Button>
+    </div>
+  );
+};
 
 export default AdminCard;
-
